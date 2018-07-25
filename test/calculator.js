d10158e136eda9100fed8d97737f065f1eac224b
olyy111 <sunrisehei@foxmail.com>
2018-07-25 22:53:30 +0800

    refactor

--- test/calculator.js
+++ test/calculator.js
@@ -4,39 +4,39 @@ var chai = require('chai')
 
 var cal = require('../lib/calculator')
 
-describe('accPlus', function () {
+describe('plus', function () {
   it('should return accurate number', function () {
-    expect(cal.accPlus(0.1, 0.2)).to.equal(0.3)
+    expect(cal.plus(0.1, 0.2)).to.equal(0.3)
   })
   it('should return accurate number', function () {
-    expect(cal.accPlus(100, 0.2)).to.equal(100.2)
+    expect(cal.plus(100, 0.2)).to.equal(100.2)
   })
   it('should return accurate number', function () {
-    expect(cal.accPlus(100, 200)).to.equal(300)
+    expect(cal.plus(100, 200)).to.equal(300)
   })
   it('should return accurate number', function () {
-    expect(cal.accPlus(0, 200)).to.equal(200)
+    expect(cal.plus(0, 200)).to.equal(200)
   })
   it('should only accept number or string', function () {
-    expect(cal.accPlus('0.1', '0.2')).to.equal(0.3)
+    expect(cal.plus('0.1', '0.2')).to.equal(0.3)
   })
 })
 
 
-describe('accPlus', function () {
+describe('plus', function () {
   it('should return accurate number', function () {
-    expect(cal.accMul(12.32, 7)).to.equal(86.24)
+    expect(cal.mul(12.32, 7)).to.equal(86.24)
   })
   it('should return accurate number', function () {
-    expect(cal.accMul(12.32, 0.7)).to.equal(8.624)
+    expect(cal.mul(12.32, 0.7)).to.equal(8.624)
   })
   it('should return accurate number', function () {
-    expect(cal.accMul(13, 14)).to.equal(182)
+    expect(cal.mul(13, 14)).to.equal(182)
   })
   it('should return accurate number', function () {
-    expect(cal.accMul(1264.985, 100)).to.equal(126498.5)
+    expect(cal.mul(1264.985, 100)).to.equal(126498.5)
   })
   it('should only accept number or string', function () {
-    expect(cal.accMul('12.32', '7')).to.equal(86.24)
+    expect(cal.mul('12.32', '7')).to.equal(86.24)
   })
 })
